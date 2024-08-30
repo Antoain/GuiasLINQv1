b66b676195993ef786b4f6f51b28fe7c7dac5579
Antony <126409554+Antoain@users.noreply.github.com>
2024-08-30 15:20:36 -0600

    Comentarios a Empleado.cs

--- IntroduccionLinq/Empleado.cs
+++ IntroduccionLinq/Empleado.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace IntroduccionLinq
 {
+    // Definición de la clase 'Empleado' dentro del espacio de nombres 'IntroduccionLinq'
     public class Empleado
     {
-
+        // Propiedad 'nombre' para almacenar el nombre del empleado
         public string nombre { get; set; }
     }
 }
