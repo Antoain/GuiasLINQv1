924f573f253da75a31c707ab8b48434ef313b256
Antony <126409554+Antoain@users.noreply.github.com>
2024-08-30 15:29:41 -0600

    Comentando Program.cs

--- IntroduccionLinq/Program.cs
+++ IntroduccionLinq/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace IntroduccionLinq
+namespace IntroduccionLinq
 {
     internal class Program
     {
@@ -8,7 +8,7 @@
             List<Casa> ListaCasas = new List<Casa>();
             List<Habitante> ListaHabitantes = new List<Habitante>();
 
-            // Llenar la lista de casas
+            // Llenar la lista de casas con ejemplos de casas
             ListaCasas.Add(new Casa
             {
                 Id = 1,
@@ -32,7 +32,7 @@
                 Ciudad = "New York"
             });
 
-            // Llenar la lista de habitantes
+            // Llenar la lista de habitantes con ejemplos de habitantes
             ListaHabitantes.Add(new Habitante
             {
                 IdHabitante = 1,
@@ -97,7 +97,7 @@
                 IdCasa = 1
             });
 
-            // Consultas con Where
+            // Consultas con Where para filtrar habitantes con edad mayor a 30 años
             IEnumerable<Habitante> ListaEdad = from ObjetoProvicional in ListaHabitantes
                                                where ObjetoProvicional.Edad > 30
                                                select ObjetoProvicional;
@@ -108,7 +108,7 @@
                 Console.WriteLine(objetoProcicional2.datosHabitante());
             }
 
-            // Consultas con Join
+            // Consultas con Join para encontrar habitantes que viven en Gothan City
             IEnumerable<Habitante> listaCasaGothan = from objetoTemporalHabitante in ListaHabitantes
                                                      join objetoTemporalCasa in ListaCasas
                                                      on objetoTemporalHabitante.IdCasa equals objetoTemporalCasa.Id
@@ -123,95 +123,95 @@
 
 
             #region FirsthAndFirsthOrDefault
-            /* Console.WriteLine("----------------------------------------------------------------------------------------------");
-            var primeraCasa = ListaCasas.First(); //esto no es linQ es  una fucnin de los Ienumarable
+            // Ejemplo comentado de cómo usar First y FirstOrDefault en LINQ
+            /*
+            var primeraCasa = ListaCasas.First(); // Método de Enumerable, no LINQ
             Console.WriteLine(primeraCasa.dameDatosCasa());
 
-            //aplicando una restriccion sin restricion lambda
+            // Aplicación de First con restricción sin usar lambdas
             Habitante personaEdad = (from variableTemporalHabitante in ListaHabitantes where variableTemporalHabitante.Edad > 25 select variableTemporalHabitante).First();
             Console.WriteLine(personaEdad.datosHabitante());
-            Console.WriteLine("---------------------------Lo mismo pero con lambdas---------------------------------------------------------");
-            var Habitante1 = ListaHabitantes.First(objectTemp => objectTemp.Edad >25);
+
+            // Usando First con expresión lambda
+            var Habitante1 = ListaHabitantes.First(objectTemp => objectTemp.Edad > 25);
             Console.WriteLine(Habitante1.datosHabitante());
 
-            // Si no tenemos el elemento que buscamos entoences la sonsulta devolvera una exepcion esto detendra el codigo en su totalidad
-
-            //Casa EdadError = (from vCasaTemp in ListaCasas where vCasaTemp.Id >10 select vCasaTemp).First() ;
-            //Console.WriteLine(EdadError.dameDatosCasa());
-
+            // Uso de FirstOrDefault para evitar excepciones cuando no se encuentra un elemento
             Casa CasaConFirsthOrDedault = ListaCasas.FirstOrDefault(vCasa => vCasa.Id > 200);
-            if (CasaConFirsthOrDedault == null ) {
-                Console.WriteLine("No existe !No hay!");
+            if (CasaConFirsthOrDedault == null) {
+                Console.WriteLine("No existe! No hay!");
                 return;
             }
-            Console.WriteLine("existe !Si existe!");
+            Console.WriteLine("existe! ¡Sí existe!");
             */
             #endregion
 
 
             #region Last
-            //Casa ultimaCasa = ListaCasas.Last(temp => temp.Id > 1);
-            //Console.WriteLine(ultimaCasa.dameDatosCasa());
-            //Console.WriteLine("_____________________________________________________");
-            //var h1 = (from objHabitante in ListaHabitantes where objHabitante.Edad > 60 select objHabitante)
-            //    .LastOrDefault();
-            //if (h1 == null)
-            //{
-            //    Console.WriteLine("Algo fallo");
-            //    return;
-            //}
-            //Console.WriteLine(h1.datosHabitante());
+            // Ejemplo comentado de cómo usar Last y LastOrDefault en LINQ
+            /*
+            Casa ultimaCasa = ListaCasas.Last(temp => temp.Id > 1);
+            Console.WriteLine(ultimaCasa.dameDatosCasa());
+
+            var h1 = (from objHabitante in ListaHabitantes where objHabitante.Edad > 60 select objHabitante).LastOrDefault();
+            if (h1 == null)
+            {
+                Console.WriteLine("Algo falló");
+                return;
+            }
+            Console.WriteLine(h1.datosHabitante());
+            */
             #endregion
 
             #region element
+            // Uso de ElementAt y ElementAtOrDefault para obtener elementos por su posición en la lista
             var terceraCasa = ListaCasas.ElementAt(2);
             Console.WriteLine($"La tercera casa es {terceraCasa.dameDatosCasa()}");
 
             var casaError = ListaCasas.ElementAtOrDefault(3);
-            if (casaError != null) { Console.WriteLine($"La tercera casa es {casaError.dameDatosCasa()}"); }
+            if (casaError != null) { Console.WriteLine($"La cuarta casa es {casaError.dameDatosCasa()}"); }
 
             var segundoHabitante = (from objetoTem in ListaHabitantes select objetoTem).ElementAtOrDefault(2);
-            Console.WriteLine($" segundo habitante es : {segundoHabitante.datosHabitante()}");
+            Console.WriteLine($"El segundo habitante es: {segundoHabitante.datosHabitante()}");
             #endregion
 
 
             #region Single
-
+            // Uso de Single y SingleOrDefault para obtener un único elemento que cumple una condición
             try
             {
                 var habitantes = ListaHabitantes.Single(variableTem => variableTem.Edad > 40 && variableTem.Edad < 70);
-                // Creando esta consulta pero con LinQ
+
                 var habitante2 = (from obtem in ListaHabitantes where obtem.Edad > 70 select obtem).SingleOrDefault();
 
-                Console.WriteLine($"habitante con menos de 20 años {habitantes.datosHabitante()}");
-                if (habitante2 != null) Console.WriteLine($"habitante con mas de 70 años {habitante2.datosHabitante()}");
+                Console.WriteLine($"Habitante con edad entre 40 y 70 años: {habitantes.datosHabitante()}");
+                if (habitante2 != null) Console.WriteLine($"Habitante con más de 70 años: {habitante2.datosHabitante()}");
             }
             catch (Exception)
             {
-                Console.WriteLine($"Ocurrio el error");
+                Console.WriteLine($"Ocurrió un error");
             }
-
             #endregion
 
-
-
             #region typeOf
+            // Uso de OfType para filtrar elementos de un tipo específico en una lista heterogénea
             var listaEmpleados = new List<Empleado>() {
-            new Medico(){ nombre= "Jorge Casa" },
-             new Enfermero(){ nombre = "Raul Blanco"}
-};
+                new Medico(){ nombre = "Jorge Casa" },
+                new Enfermero(){ nombre = "Raul Blanco"}
+            };
 
             var medico = listaEmpleados.OfType<Medico>();
             Console.WriteLine(medico.Single().nombre);
-
             #endregion
 
             #region OrderBYDescending()
-            /* var listaEdad = ListaHabitantes.OrderByDescending(x => x.Edad);
+            // Ejemplo comentado de cómo ordenar en orden descendente usando LINQ y lambdas
+            /*
+            var listaEdad = ListaHabitantes.OrderByDescending(x => x.Edad);
             foreach (Habitante h in listaEdad) { 
                 Console.WriteLine(h.datosHabitante());
             }
-            Console.WriteLine("-------------------------------------------");
+            
             var ListaEdad2 = from h in ListaHabitantes orderby h.Edad descending select h;
             foreach (Habitante h in ListaEdad2)
             {
@@ -220,19 +220,14 @@
             */
             #endregion
 
-
             #region Thenby
-            
-
+            // Uso de ThenBy y ThenByDescending para realizar ordenaciones secundarias
             var habitantes3 = ListaHabitantes.OrderBy(x => x.Edad).ThenBy(x => x.Nombre);
 
             foreach (var h in habitantes3)
             {
                 Console.WriteLine(h.datosHabitante());
             }
-
-
-
 
             var edadATA = from h in ListaHabitantes orderby h.Edad, h.Nombre descending select h;
 
@@ -250,21 +245,13 @@
                 Console.WriteLine(h.datosHabitante());
             }
 
-
-
-
             var lista4 = from h in ListaHabitantes orderby h.Edad, h.Nombre ascending select h;
 
             foreach (var h in lista4)
             {
                 Console.WriteLine(h.datosHabitante());
             }
-
-
-
-
-
+            #endregion
         }
-
     }
 }
