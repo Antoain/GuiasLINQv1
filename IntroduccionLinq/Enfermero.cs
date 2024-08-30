e6558f053461f5a0d7c3a9bbab4540cb65ea7c21
Antony <126409554+Antoain@users.noreply.github.com>
2024-08-30 15:21:44 -0600

    Comentando Enfermero.cs

--- IntroduccionLinq/Enfermero.cs
+++ IntroduccionLinq/Enfermero.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace IntroduccionLinq
 {
+    // La clase 'Enfermero' hereda de la clase 'Empleado'
     public class Enfermero : Empleado
     {
+        // Propiedad 'nombre' para almacenar el nombre del enfermero
         public string nombre { get; set; }
     }
 }
